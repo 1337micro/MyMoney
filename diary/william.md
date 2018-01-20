5f248119f124685652b19c96bb82db38d810c0d5
William Prioriello <williamap@gmail.com>
2018-01-19 23:00:29 -0500

    Add system look and feel

--- diary/william.md
+++ diary/william.md
@@ -6,4 +6,5 @@ William Prioriello 27080956
 1/19/2018
 Looked over Noemi's code, made changes,
 created Constants Class
+add system look and feel to Swing application
 pushed to upstream git repository
---
+++ test/ApplicationLayoutTest.java
@@ -0,0 +1,15 @@
+package test;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class ApplicationLayoutTest{
+
+    @Test
+    void basic_test(){
+        assertEquals(1,1);
+    }
+   // public static void main( final String[] args ){}
+}
