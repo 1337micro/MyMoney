316783ed7708782f7c43bbef45d9dc04afba76b0
William Prioriello <williamap@gmail.com>
2018-01-19 19:25:35 -0500

    Add system look and feel

--- source/Main.java
+++ source/Main.java
@@ -1,6 +1,16 @@
+import javax.swing.*;
+
 public class Main{
     public static void main(String[] args) {
-//displaying the window
+        // Use the system theme, this is purely aesthetic
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        //displaying the window
         ApplicationLayout applicationLayout = new ApplicationLayout();
         applicationLayout.setVisible(true);
     }
