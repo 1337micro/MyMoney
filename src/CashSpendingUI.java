0b8200e13ef22b5d8c9a2ef157c1db7f1813a76e
William Prioriello <williamap@gmail.com>
2018-01-23 11:19:33 -0500

    Add comments to CashSpendingUI class

--- src/CashSpendingUI.java
+++ src/CashSpendingUI.java
@@ -8,17 +8,21 @@ import java.util.Optional;
 
 import static src.CashSpending.ExpenditureType.*;
 public class CashSpendingUI implements ActionListener {
-    JPanel thePanel;
-    private final int FIELD_SIZE = 10;
+    JPanel thePanel; // the panel containing all our JTextFields for CashSpending feature
+    private final int FIELD_SIZE = 10; // number of columns in our GUI text fields
 
-    //list of the JTextField fields above, added in the constructor
+    //list of the JTextField fields having the total amount spent in a particular category.
     private  ArrayList<JTextField> listOfMoneySpentFields = new ArrayList<>();
+
     public CashSpendingUI(){
-        addJTextFieldToListAndSetNotEditable(listOfMoneySpentFields);//add n jtextfields to listOfMoneySpentFields; where n is numbers of Expidenture Types
+        addJTextFieldToListAndSetNotEditable(listOfMoneySpentFields);//add n jtextfields to listOfMoneySpentFields; where n is numbers of Expenditure Types
     }
 
 
-    private void addJTextFieldToListAndSetNotEditable(List<JTextField> list, JTextField... jTextField){
+    /**Add n jtextfields to list; where n is numbers of Expenditure Types
+     * @param list that will be populated by JTextFields. We add a JTextField for each enum in ExpenditureType
+     */
+    private void addJTextFieldToListAndSetNotEditable(List<JTextField> list){
         //no fields supplied, use ordering from enum type
         for(int i =0; i<CashSpending.ExpenditureType.values().length; i++) {
             JTextField jText = new JTextField(FIELD_SIZE);
@@ -27,6 +31,9 @@ public class CashSpendingUI implements ActionListener {
         }
     }
 
+    /**Update the listOfMoneySpentFields to reflect the total money spent after a user enters a new expenditure
+     *
+     */
     private void updateExpensesFields(){
         for(int i = 0; i<CashSpending.ExpenditureType.values().length; i++){
             CashSpending.ExpenditureType expenditureType = CashSpending.ExpenditureType.values()[i];
@@ -38,6 +45,10 @@ public class CashSpendingUI implements ActionListener {
             }
         }
     }
+
+    /** Add the necessary SWING elements to the CashSpending panel named "thePanel"
+     *
+     */
     private void buildCashSpendingDisplayPanel(){
         List<JTextField> listOfExpenditureTypes = new ArrayList<>(CashSpending.ExpenditureType.values().length);
         addJTextFieldToListAndSetNotEditable(listOfExpenditureTypes);
@@ -61,6 +72,12 @@ public class CashSpendingUI implements ActionListener {
         thePanel.add(addExpense);
         thePanel.setVisible(false);
     }
+
+    /**Add any JPanel to our application layout
+     *
+     * @param jPanel Any SWING jpanel
+     * @param applicationLayout the application layout object
+     */
     public void addPanelToLayout(JPanel jPanel, ApplicationLayout applicationLayout){
         applicationLayout.add(jPanel);
     }
@@ -69,7 +86,10 @@ public class CashSpendingUI implements ActionListener {
 
     }
 
-
+    /** Create a popup asking the user to input the amount of money they spent on a particular type of expense
+     * @param expenditureType type of expense
+     * @return Double; the amount of money spent on this expenditureType
+     */
     public Double showCashSpendingAmountOfMoneyDialog(CashSpending.ExpenditureType expenditureType){
         Optional<String> inputAmountSpent = Optional.ofNullable(JOptionPane
                 .showInputDialog("How much money did you spend on " + expenditureType.toString().toLowerCase() + " ?"));
@@ -85,6 +105,10 @@ public class CashSpendingUI implements ActionListener {
         return amountSpent;
     }
 
+    /**
+     * Button listening for a click on the "Cash Spending" button. It will show or hide the CashSpending panel "thePanel"
+     * @param e SWING argument
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         if(thePanel == null) {
@@ -99,7 +123,14 @@ public class CashSpendingUI implements ActionListener {
         }
     }
 
+    /**
+     * A class that will act as a listener for the "AddExpense" button on the CashSpending panel "thePanel" variable
+     */
     private class AddExpenseListener implements ActionListener {
+        /** Create a popup asking the user to select a particular type of expense
+         * @param applicationLayout
+         * @return the {@link src.CashSpending.ExpenditureType} that the user selected
+         */
         private CashSpending.ExpenditureType showCashSpendingExpenditureDialog(ApplicationLayout applicationLayout) {
             final Integer selection = (JOptionPane.showOptionDialog(applicationLayout,"Choose your Expenditure Type: ",
                     "Type of Expense? ",
@@ -112,6 +143,13 @@ public class CashSpendingUI implements ActionListener {
             return CashSpending.ExpenditureType.values()[selection];
 
         }
+
+        /**
+         * A method to handle the selection made after showCashSpendingExpenditureDialog was called
+         * We ask how much the user spent on the selection type
+         * @param selection the type of expenditure that was selected
+         * @return
+         */
         private Double handleSelectionOfExpenditureType(CashSpending.ExpenditureType selection){
             Double amountSpent;
             switch (selection){
@@ -133,6 +171,10 @@ public class CashSpendingUI implements ActionListener {
             return amountSpent;
         }
 
+        /**
+         * "AddExpense" Button listening for a click. It will bring up a dialog prompt asking what type of expenditure
+         * @param e SWING argument
+         */
         @Override
         public void actionPerformed(ActionEvent e) {
             final CashSpending.ExpenditureType expenditureType = showCashSpendingExpenditureDialog(Main.getApplicationLayout());
