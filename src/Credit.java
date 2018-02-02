b27b64577ac716fa8eb28a759465fdfb8d7fb34e
Noémi Lemonnier <noemilemonnier@noemis-mbp.wireless.concordia.ca>
2018-02-01 19:50:47 -0500

    updated

--- src/Credit.java
+++ src/Credit.java
@@ -3,24 +3,24 @@ package src;
 public class Credit extends Cards{
 
 	double limit;
-	double moneyOwed;
+	double moneySpent;
 
 	public Credit() {
 		type=CardType.CREDIT;
 		accNb = 0;
 		cardNumber=0;
 		limit=0;
-		moneyCurrent=0;
-		moneyOwed=moneyCurrent-limit;
+		moneySpent=0;
+		moneyAvailable=limit-moneySpent;
 	}
 
-	public Credit(Cards.CardType type, int accNb, int cardNumber, double moneyCurrent, double limit) {
+	public Credit(Cards.CardType type, int accNb, int cardNumber, double moneySpent, double limit) {
 		this.type=CardType.CREDIT;
 		this.accNb = accNb;
 		this.cardNumber=cardNumber;
 		this.limit =limit;
-		this.moneyCurrent=moneyCurrent;
-		this.moneyOwed=moneyCurrent-limit;
+		this.moneySpent=moneySpent;
+		this.moneyAvailable=limit-moneySpent;
 	}
 
 	//need to create setters and getter for limit and moneyOwed. the others can be from the super class
@@ -34,15 +34,15 @@ public class Credit extends Cards{
 		this.limit=limit;
 	}
 	/**
-	 * setters for the money currently owed by the card owner
-	 * @param double moneyOwed
+	 * setting the money available on the credit card
+	 * @param moneyAvailable
 	 */
-	public void setMoneyOwed(double moneyOwed) {
-		if(limit-moneyOwed==moneyCurrent)
-			this.moneyOwed=moneyOwed;
+	public void setMoneyOwed(double moneyAvailable) {
+		if(limit-moneySpent==moneyAvailable)
+			this.moneyAvailable=moneyAvailable;
 		else {
-			this.moneyOwed=moneyOwed;
-			moneyCurrent=limit-moneyOwed;
+			this.moneyAvailable=moneyAvailable;
+			moneyAvailable=limit-moneySpent;
 		} 
 
 	}
@@ -71,12 +71,21 @@ public class Credit extends Cards{
 	public double getLimit() {
 		return limit;
 	}
+	
+	public double getMoneySpent() {
+		return moneySpent;
+	}
+
+	public void setMoneySpent(double moneySpent) {
+		this.moneySpent = moneySpent;
+	}
+
 	/**
 	 * getter for the money owed by the owner of the card
 	 * @return double of the amount of money owed by the owner of the card
 	 */
-	public double getMoneyOwed() {
-		return moneyOwed;
+	public double getMoneyAvailable() {
+		return moneyAvailable;
 	}
 }
 
