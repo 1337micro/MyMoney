a654a89cb63cc7cefd500faa8b123d7d100baa97
Noémi Lemonnier <noemilemonnier@noemis-mbp.wireless.concordia.ca>
2018-02-14 15:44:36 -0500

    update

---
+++ iteration1/ApplicationLayoutTest.java
@@ -0,0 +1,14 @@
+package src;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class ApplicationLayoutTest{
+
+    @Test
+    void basic_test(){
+        assertEquals(1,1);
+    }
+}
---
+++ iteration1/Credit.java
@@ -0,0 +1,99 @@
+//-------------------------------------------------------
+//For Comp 354 Section PP - Winter 2018
+//Iteration 1: Genevieve Plante-Brisebois 40003112
+//Help received from the Programmer Organizer: Noémi Lemonnier 40001085
+//Description: implements the interface cards and serves the purpose of implementing the 
+//              specifics of the cards of type CREDIT
+//--------------------------------------------------------
+
+package src;
+
+// Credit Card Class that is a subclass of Cards
+public class Credit implements Cards{
+	
+	// Credit Cards have a few more variables than the Debit Cards
+	CardType type;
+	int accNb;
+	int cardNumber;
+	double limit;
+	double moneySpent;
+	double moneyAvailable;
+	
+	public Credit() {
+		type=CardType.CREDIT;
+		accNb = 0;
+		cardNumber=0;
+		limit=0;
+		moneySpent=0;
+		moneyAvailable=limit-moneySpent;
+	}
+
+	public Credit(CardType cardType, int accNb, int d, double moneySpent, double limit) {
+		this.type=CardType.CREDIT;
+		this.accNb = accNb;
+		this.cardNumber=d;
+		this.limit =limit;
+		this.moneySpent=moneySpent;
+		this.moneyAvailable=limit-moneySpent;
+	}
+
+	@Override
+	public CardType getType() {
+		return this.type;
+	}
+
+	@Override
+	public void setType(CardType type) {
+		this.type = type;
+
+	}
+
+	@Override
+	public int getAccNb() {
+		return accNb;
+	}
+	@Override
+	public void setAccNb(int accNb) {
+		this.accNb = accNb;
+	}
+	@Override
+	public int getCardNumber() {
+		return cardNumber;
+	}
+	@Override
+	public void setCardNumber(int cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	@Override
+	public double getMoneySpent() {
+		return moneySpent;
+	}
+	@Override
+	public void setMoneySpent(double moneySpent) {
+		this.moneySpent = moneySpent;
+	}
+	@Override
+	public void setLimit(double limit) {
+		this.limit=limit;
+	}
+	@Override
+	public double getLimit() {
+		return limit;
+	}
+
+	@Override
+	public double getMoneyAvailable() {
+		return moneyAvailable;
+	}
+
+	@Override
+	public void setMoneyAvailable(double moneyAvailable) {	
+		if(limit-moneySpent==moneyAvailable)
+			this.moneyAvailable=moneyAvailable;
+		else {
+			this.moneyAvailable=moneyAvailable;
+			moneyAvailable=limit-moneySpent;
+		} 
+	}
+
+}
